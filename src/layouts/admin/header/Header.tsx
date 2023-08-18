bcfdd3950ff8b581c1824e4face354b6c83649af
daidv <dvdai63@gmail.com>
2023-08-18 07:12:37 +0700

    add structure repo

---
+++ src/layouts/admin/header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.scss';
+
+function Header() {
+  return (
+    <header className='admin-header'>
+      Hello
+    </header>
+  );
+}
+
+export default Header;
