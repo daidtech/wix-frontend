6e7e384fba48e3f42a806904934b8a69ec1a7647
daidv <dvdai63@gmail.com>
2023-08-23 07:19:01 +0700

    udpate sidebar

--- src/layouts/admin/Sidebar/index.tsx
+++ src/layouts/admin/Sidebar/index.tsx
@@ -8,23 +8,17 @@ function Sidebar() {
     <div className='sidebar'>
       <Accordion defaultActiveKey="0">
         <Accordion.Item eventKey="0">
-          <Accordion.Header>Accordion Item #1</Accordion.Header>
+          <Accordion.Header>Store Products</Accordion.Header>
           <Accordion.Body>
-            <Subitem link={'google.com'}> Activity </Subitem>
             <Subitem link={'google.com'}> Products </Subitem>
+            <Subitem link={'google.com'}> Inventory </Subitem>
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="1">
-          <Accordion.Header>Accordion Item #2</Accordion.Header>
+          <Accordion.Header>Orders</Accordion.Header>
           <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
+          <Subitem link={'google.com'}> Orders </Subitem>
+            <Subitem link={'google.com'}> Abandoned Carts </Subitem>          </Accordion.Body>
         </Accordion.Item>
       </Accordion>
     </div>
