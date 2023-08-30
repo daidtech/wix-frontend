f839ae1d9d036ae40be72ff1aecc34557c27c081
daidv <dvdai63@gmail.com>
2023-08-30 08:06:27 +0700

    add react router

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,27 @@ import './index.css';
 import App from './views/App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
