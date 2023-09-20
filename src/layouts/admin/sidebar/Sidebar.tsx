52cb2001dc8cf081f83c165f1de54cd47119f9cc
daidv <dvdai63@gmail.com>
2023-09-21 06:56:46 +0700

    update sidebar

--- src/layouts/admin/sidebar/Sidebar.tsx
+++ src/layouts/admin/sidebar/Sidebar.tsx
@@ -7,18 +7,19 @@ function Sidebar() {
   return (
     <div className='sidebar'>
       <Accordion defaultActiveKey="0">
-        <Accordion.Item eventKey="0">
+        <Accordion.Item bsPrefix="sidebar-accordion" eventKey="0">
           <Accordion.Header>Store Products</Accordion.Header>
           <Accordion.Body>
             <Subitem link={'/products'}> Products </Subitem>
             <Subitem link={'google.com'}> Inventory </Subitem>
           </Accordion.Body>
         </Accordion.Item>
-        <Accordion.Item eventKey="1">
+        <Accordion.Item bsPrefix="sidebar-accordion" eventKey="1">
           <Accordion.Header>Orders</Accordion.Header>
           <Accordion.Body>
-          <Subitem link={'google.com'}> Orders </Subitem>
-            <Subitem link={'google.com'}> Abandoned Carts </Subitem>          </Accordion.Body>
+            <Subitem link={'google.com'}> Orders </Subitem>
+            <Subitem link={'google.com'}> Abandoned Carts </Subitem>
+          </Accordion.Body>
         </Accordion.Item>
       </Accordion>
     </div>
